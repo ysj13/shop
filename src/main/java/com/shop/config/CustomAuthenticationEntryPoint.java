edc45518528c2f05934aacf59185498f5d91a632
sj <ysj13@kakao.com>
2022-11-09 18:07:32 +0900

    사용자 요청 검증

---
+++ src/main/java/com/shop/config/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package com.shop.config;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    // 인증되지 않은 사용자 요청 시 에러 발생
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
